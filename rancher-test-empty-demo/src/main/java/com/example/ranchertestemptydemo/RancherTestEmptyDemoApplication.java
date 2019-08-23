981d27423b40853ee39a06c385b798b2e4e1f5af
yuanqing <debugs7@163.com>
2019-08-23 13:31:49 +0800

    更新rancher测试程序vcs文件

---
+++ rancher-test-empty-demo/src/main/java/com/example/ranchertestemptydemo/RancherTestEmptyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ranchertestemptydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RancherTestEmptyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RancherTestEmptyDemoApplication.class, args);
+    }
+
+}
