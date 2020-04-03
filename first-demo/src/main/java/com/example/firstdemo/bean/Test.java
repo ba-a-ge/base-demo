fbd397bb543a19ff855b1aff24cee17b0bebb07e
yuanqing <debugs7@163.com>
2020-04-03 10:54:09 +0800

    分支测试 test1

--- first-demo/src/main/java/com/example/firstdemo/bean/Test.java
+++ first-demo/src/main/java/com/example/firstdemo/bean/Test.java
@@ -3,7 +3,7 @@ package com.example.firstdemo.bean;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("I am master");
+        System.out.println("I am test");
     }
 
 }
